35678de144c047118c1931a255346036e9ae8b9c
hyird <31695447+hyird@users.noreply.github.com>
2020-10-05 12:56:08 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -3,6 +3,7 @@
 echo "Apply custom.sh"
 
 sed -i 's/DEPENDS.*/& \+luci-i18n-mwan3-zh-cn/g'  package/lean/luci-app-syncdial/Makefile
+rm -rf package/feeds/routing/olsrd/patches/012-gpsd.patch
 rm -rf package/lean/default-settings/files/zzz-default-settings
 wget https://raw.githubusercontent.com/coolsnowwolf/lede/99222f49fd9f63eef575ac34f251280e6e97f3b3/package/lean/default-settings/files/zzz-default-settings -O package/lean/default-settings/files/zzz-default-settings
 
